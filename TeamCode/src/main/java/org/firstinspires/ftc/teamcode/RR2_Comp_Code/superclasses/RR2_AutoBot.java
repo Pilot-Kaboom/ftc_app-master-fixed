a1c879c93a7cbd78cfc1f821013a65e65e9848af
Keith Harder <keithharder10@gmail.com>
2018-12-11 21:50:40 -0600

    auto calibration 1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR2_Comp_Code/superclasses/RR2_AutoBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR2_Comp_Code/superclasses/RR2_AutoBot.java
@@ -32,9 +32,14 @@ public abstract class RR2_AutoBot extends SuperSuperClass {
         parameters.vuforiaLicenseKey = "AXk8m93/////AAAAmSo/yH0caUMsqpjNljWxJF8YX6vQ0htlGYE6tx1nTRyclEBgdBc6yxK8FcCcBaKOJ4vuWvJSuv7t5CMUEA9Zo0NrKut9gg+VZovv+S1NoSnG7aQHhCTaDTlhaZ12MRnRxbkEj72yfSdQhZRbT+MGJ7pBGnvWtOSH/YmeK6bffeICGgdjZz9PoHg8AuWBMvWu6vezXnbAX4IzX1KTmQs8KU2ylFsNRzNhErlnZwDw4vTqRPhk6hhKVOV4iaQup/2CgY7fwblJIV/xKhJMMr7GYi8FPDlYctv4dAONrCPqUOLY0MqxhKf++TqI/kNC+kL8Xa/koNY6Cg+2LT9QEn5J819YnhtcwMzIoHzIRPb/j7NM";
 
 
-        vision = new MasterVision(parameters, hardwareMap, true, MasterVision.TFLiteAlgorithm.INFER_NONE);
+        vision = new MasterVision(parameters, hardwareMap, true, MasterVision.TFLiteAlgorithm.INFER_RIGHT);
         vision.init();// enables the camera overlay. this will take a couple of seconds
         vision.enable();// enables the tracking algorithms. this might also take a little time
+
+        telemetry.addData("goldPosition is c", goldPosition == SampleRandomizedPositions.CENTER);
+        telemetry.addData("goldPosition is l", goldPosition == SampleRandomizedPositions.LEFT);
+        telemetry.addData("goldPosition is R", goldPosition == SampleRandomizedPositions.RIGHT);
+        telemetry.update();
 
 
 
